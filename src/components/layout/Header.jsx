0497040b0534f08ae980a25a7ab19ab54ef60924
RAVI YADAV <r4852453761@gmail.com>
2026-09-24 09:11:51 +0530

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -124,69 +124,7 @@ const Header = ({ setBg }) => {
         </NavLink>
 
         {/* Background Buttons */}
-        <div className="flex gap-1 sm:gap-2">
-
-          {/* <button
-            onClick={() => setBg("bg-black")}
-            className="
-              bg-black
-              text-white
-              rounded
-              px-2 py-1
-              sm:px-3
-              text-[10px] sm:text-sm
-              whitespace-nowrap
-            "
-          >
-            Black
-          </button> */}
-
-          <button
-            onClick={() => setBg("bg-white")}
-            className="
-              bg-white
-              text-black
-              rounded
-              px-2 py-1
-              sm:px-3
-              text-[10px] sm:text-sm
-              whitespace-nowrap
-            "
-          >
-            White
-          </button>
-
-          <button
-            onClick={() => setBg("bg-green-400")}
-            className="
-              bg-green-400
-              text-black
-              rounded
-              px-2 py-1
-              sm:px-3
-              text-[10px] sm:text-sm
-              whitespace-nowrap
-            "
-          >
-            Green
-          </button>
-
-          {/* <button
-            onClick={() => setBg("bg-blue-400")}
-            className="
-              bg-blue-400
-              text-black
-              rounded
-              px-2 py-1
-              sm:px-3
-              text-[10px] sm:text-sm
-              whitespace-nowrap
-            "
-          >
-            Blue
-          </button> */}
-
-        </div>
+     
 
       </nav>
 
